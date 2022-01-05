0b11c58878a8123b8ef766816f5a398472c15422
jagjang <eliceaitrack@gmail.com>
2022-01-05 15:40:05 +0900

    change username

--- routes/home.js
+++ routes/home.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const db = require('../db');
 
 router.get('/', (req, res) => {
-    const query1 = 'select idx, title, author, DATE_FORMAT(writeTime, \'%y/%m/%d\') as day from posts order by idx desc';
-    db.askQuery(query1).then((result) => {
+    const query = 'select idx, title, author, DATE_FORMAT(writeTime, \'%y/%m/%d\') as day from posts order by idx desc';
+
+    db.askQuery(query).then((result) => {
         res.render('index', { title: '게시판', data: result});
     }).catch((error) => {
         console.log(error);
--- routes/post.js
+++ routes/post.js
@@ -2,39 +2,18 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db');
 
-// Promise 비동기 처리
 router.get('/:idx', (req, res) => {
     let idx = req.params.idx;
 
     const query1 = `select idx, title, author, DATE_FORMAT(writeTime, '%y/%m/%d') as day, content from posts where idx='${idx}'`;
     const query2 = "select count(*) from posts";
 
+    
     db.askQuery(query1).then((result1) => {
         res.render('post', {title: '게시글', data: result1[0]});
     }).catch((error) => {
         console.log(error);
     });
 });
-
-/* 콜백함수 이용(원본)
-router.get('/post/:idx', (req, res) => {
-    let idx = req.params.idx;
-    let post = {};
-
-    let qry = `select idx, title, author, DATE_FORMAT(writeTime, '%y/%m/%d') as day, content from posts where idx='${idx}'`;
-    db.query(qry, (error, result, fields) => {
-        if(error) {
-            console.log(`failed to get post ${idx}`);
-            console.log(error);
-        }
-        else {
-            post = result;
-        }
-
-        // 페이지 렌더링
-        res.render('post', {title: '게시글', data: post[0]});
-    });
-});
-*/
 
 module.exports = router;
