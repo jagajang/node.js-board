8d7c497c41255ff42eae27bed427c7b4b20db5fc
jagajang <eliceaitrack@gmail.com>
2022-01-05 21:48:33 +0900

    글쓰기 기능 구현

--- query.sql
+++ query.sql
@@ -1,9 +1,11 @@
+-- 데 생성
+
 create database board;
 use board;
 
 create table posts(
-    idx INT not null PRIMARY KEY,
-    title VARCHAR(30) not null,
+    idx INT not null AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(50) not null,
     author VARCHAR(30) not null,
     writeTime DATETIME not null,
     content TEXT not null,
--- routes/write.js
+++ routes/write.js
@@ -2,17 +2,23 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db');
 
+const printTime = () => {
+    return new Date(new Date() + 3240 * 10000).toISOString().replace("T", " ").replace(/\..*/, '');
+};
+
 router.get('/', (req, res) => {
     res.render('write', {title: '글쓰기'});
 });
 
 router.post('/', (req, res) => {
-    console.log(req);
-    console.log("title: "+req.body.title);
-    console.log("passwd: "+req.body.passwd);
-    console.log("content: "+req.body.content);
+    const query = `insert into posts(title, author, writeTime, content, passwd)
+        values("${req.body.title}", "somebody", "${printTime()}", "${req.body.content}", "${req.body.passwd}")`;
     
-    res.redirect('/');
+    db.askQuery(query).then(() => {
+        res.redirect('/');
+    }).catch((error) => {
+        console.log(error);
+    });
 });
 
 module.exports = router;
