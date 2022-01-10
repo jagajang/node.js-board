bd442cf4e74344c2a2b717ed43241cd4931ab64d
jagajang <eliceaitrack@gmail.com>
2022-01-10 17:46:16 +0900

    이전글, 다음글 정보 DB에 포함

--- query.sql
+++ query.sql
@@ -2,10 +2,15 @@ create database board;
 use board;
 
 create table posts(
-    idx INT not null AUTO_INCREMENT PRIMARY KEY,
+    idx INT not null AUTO_INCREMENT,
     title VARCHAR(50) not null,
     author VARCHAR(30) not null,
     writeTime DATETIME not null,
     content TEXT not null,
-    passwd VARCHAR(30) not null
+    passwd VARCHAR(30) not null,
+
+    pre_idx INT,
+    nex_idx INT,
+
+    PRIMARY KEY ( idx )
 );
--- routes/read.js
+++ routes/read.js
@@ -5,7 +5,7 @@ const db = require('../db');
 router.get('/:idx', (req, res) => {
     let idx = req.params.idx;
 
-    const query1 = `select idx, title, author, DATE_FORMAT(writeTime, '%y/%m/%d') as day, content from posts where idx='${idx}'`;
+    const query1 = `select idx, title, author, DATE_FORMAT(writeTime, '%y/%m/%d') as day, content, pre_idx, nex_idx from posts where idx='${idx}'`;
     const query2 = "select count(*) as cnt from posts";
 
     db.askQuery(query1+";"+query2).then((result) => {
